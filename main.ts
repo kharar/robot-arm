1baa83e4488dab21f34c6c006352c8e232f65eb8
Martin Axelsen <kharar@gmail.com>
2020-08-28 23:35:25 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,12 @@ function set_robot_arm () {
     robotbit.GeekServo(robotbit.Servos.S1, 60 + (klo_pos - arm_pos))
     robotbit.GeekServo(robotbit.Servos.S2, arm_pos - 30)
 }
-let arm_pos = 0
 let klo_pos = 0
+let arm_pos = 0
 let delaytime = 5
 let arm_max = 90
-let klo_max = 90
+arm_pos = 0
+let klo_max = 120
 klo_pos = 0
 set_robot_arm()
 basic.forever(function () {
